aac6cb1994d87e6c2fbf24fe1d82a6c1ed77487a
Tusk <Tuskman045@gmail.com>
2025-02-11 08:56:22 -0700

    Add randomness

--- ArrayExamples/arrayExamples.vb
+++ ArrayExamples/arrayExamples.vb
@@ -8,7 +8,8 @@ Module arrayExamples
 
     Sub Main()
 
-        multiDimentionalArrays()
+        'multiDimentionalArrays()
+        testRandomness()
 
 
     End Sub
@@ -33,18 +34,43 @@ Module arrayExamples
     'End Sub
 
     Sub multiDimentionalArrays()
-        Dim students(14, 2) As Single
-        Dim classes() As String = {"Math", "History", "Science"}
-        Dim coolStuff(,) As Integer = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
-        Dim ThreeDArray(,,) As Integer = {{{1}, {2}, {3}}, {{4}, {5}, {6}}, {{7}, {8}, {9}}}
+        'Dim students(14, 2) As Single
+        'Dim classes() As String = {"Math", "History", "Science"}
+        'Dim coolStuff(,) As Integer = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+        'Dim ThreeDArray(,,) As Integer = {{{1}, {2}, {3}}, {{4}, {5}, {6}}, {{7}, {8}, {9}}}
 
 
-        students(5, 2) = 52
-        students(5, 1) = 68
-        students(5, 0) = 100
+        'students(5, 2) = 52
+        'students(5, 1) = 68
+        'students(5, 0) = 100
 
 
-        Console.WriteLine()
+        'Console.WriteLine()
     End Sub
+
+    Sub testRandomness()
+        Dim beanCounter(20) As Integer
+
+        For i = 1 To 10000
+            beanCounter(randomNumberBetween(10, 3)) += 1
+
+        Next
+
+        For i = LBound(beanCounter) To UBound(beanCounter)
+
+            Console.WriteLine($"{CStr(i).PadLeft(4)} hit {CStr(beanCounter(i)).PadLeft(4)} times!")
+        Next
+
+    End Sub
+
+    Function randomNumberBetween(max As Integer, min As Integer) As Integer
+        Dim temp As Single
+
+        Randomize()
+        temp = Rnd()
+        temp *= max - min
+        temp += min
+        Return CInt(temp)
+    End Function
 
 End Module
